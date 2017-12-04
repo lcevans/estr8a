b5651ecbe344426971e68411505b284c21b40f2e
Chris Brewer <chris.brewer@adroll.com>
2017-12-04 14:15:01 -0600

    Add digits.js which defines global digits array as an array of chip font characters.

---
+++ public/js/digits.js
@@ -0,0 +1,119 @@
+// Hex digits 0-F are hard coded into memory:
+// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#font
+
+var stringToChipFont = (string) => {
+    var rows = string.trim().split("\n");
+    var bytes = new Uint8Array(rows.length);
+    for (var row =0; row < rows.length; row++) {
+        value = 0;
+        for (var i = 0; i < 4; i++) {
+            value = (value << 1);
+            // This will just be undefined if the row doesn't have a full 4 characters, which is fine.
+            if (rows[row][i] === '*') {
+                value |= 1;
+            }
+        }
+        // Chip fonts only use the 4 most significant bits of each byte.
+        bytes[row] = value << 4;
+    }
+    return bytes;
+}
+
+// `*` indicate 1 bits, and spaces are 0 bits. Each row can only have 4 bits.
+var digits = [`
+****
+*  *
+*  *
+*  *
+****
+`,`
+  *
+ **
+  *
+  *
+ ***
+`,`
+****
+   *
+****
+*
+****
+`,`
+****
+   *
+****
+   *
+****
+`,`
+*  *
+*  *
+****
+   *
+   *
+`,`
+****
+*
+****
+   *
+****
+`,`
+****
+*
+****
+*  *
+****
+`,`
+****
+   *
+  *
+ *
+ *
+`,`
+****
+*  *
+****
+*  *
+****
+`,`
+****
+*  *
+****
+   *
+****
+`,`
+****
+*  *
+****
+*  *
+*  *
+`,`
+***
+*  *
+***
+*  *
+***
+`,`
+****
+*
+*
+*
+****
+`,`
+***
+*  *
+*  *
+*  *
+***
+`,`
+****
+*
+****
+*
+****
+`,`
+****
+*
+****
+*
+*
+`].map(stringToChipFont);
