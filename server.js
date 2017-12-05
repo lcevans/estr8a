7d84150ae8a0db97c8727958b9a5209e0c8d7586
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2017-12-05 14:02:28 -0200

    Serve our games

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.use('/api/games/', express.static('games'));
 
 app.listen(3000);
 console.log("Listening on http://localhost:3000/");
