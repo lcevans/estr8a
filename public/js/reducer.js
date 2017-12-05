da798252b24f581cb3117ebe1c8550b43ae6cb80
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2017-12-05 16:02:10 -0200

    Pure function of the state

---
+++ public/js/reducer.js
@@ -0,0 +1,43 @@
+const defaultState = {
+    memory: new Uint8Array(0x1000),
+    register: new Uint8Array(0x10),
+    stack: new Uint8Array(0x10),
+    programCounter: 0x200,
+    stackPointer: 0x0,
+    iRegister: 0x0,
+    screen: null /* dunno yet */,
+    // the keyboard could be updated separately
+    // or else read from the keyboard itself
+    keyboard: new Uint8Array(0x10),
+};
+
+
+const step = (state = defaultState, instruction) => {
+    const opCode = (instruction & 0xF000) >> 8;
+    switch(opCode) {
+        case 0x0: {
+            switch(instruction) {
+                // CLS
+                case 0x0e0:
+                    return Object.merge(state, {
+                        screen: null,
+                        programCounter: state.programCounter++,
+                    });
+            }
+        }
+        // JP addr
+        case 0x1: {
+            const addr = instruction & 0x0fff;
+            return Object.merge(state, {
+                programCounter: addr
+            });
+        }
+            // ... way more horrible code
+    }
+};
+
+
+module.exports = {
+    defaultState,
+    step,
+}
