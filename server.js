004eae2498273f6fc894b32390330cd78eeef93a
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2017-12-05 14:42:28 -0200

    Return a list of games from the api

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var app = express();
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+
+const GAMES_DIR = 'games/'
 
 app.use(express.static('public'));
-app.use('/api/games/', express.static('games'));
+app.use('/api/games/', express.static(GAMES_DIR, {index: false}));
+
+app.get('/api/games/', (req, res) => {
+    fs.readdir(GAMES_DIR, (err, files) => {
+        if (err) {
+            res.status(500);
+            res.send({error: err})
+        } else {
+            res.send({data: files})
+        }
+    });
+});
+
 
 app.listen(3000);
 console.log("Listening on http://localhost:3000/");
