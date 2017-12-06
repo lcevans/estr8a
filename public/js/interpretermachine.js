c2509adb3cf53909bebb8d52c4faf32ebe26a812
Ezequiel Pozzo <ezequiel.pozzo@adroll.com>
2017-12-06 13:25:13 -0300

    Modularize and connect reducer

--- public/js/interpretermachine.js
+++ public/js/interpretermachine.js
@@ -1,34 +1,22 @@
 class InterpreterMachine {
     constructor(screenSize) {
-        this.memory = new Uint8Array(4096); // Main memory
-        this.V = new Uint8Array(16); // General purpose registers
-        this.I = 0;    // Instruction register
-        this.S = new Uint16Array(16); // Stack
-        this.SP = -1;  // Stack pointer
-        this.PC = 512; // Initial address of the program
-        this.DT = 0;   // Delay timer
-        this.ST = 0;   // Sound timer
-        this.screen = new Uint8Array(screenSize);
-        for (i=0; i<=0xF; i++) {
-            this[`V${i.toString(16)}`] = 0;
-        }
+        this.state = reducerModule.step();
     }
-    moveToNextInstruction() {
-        this.PC += 2;
+    fetchInstruction() {
+        let instruction = this.memory[this.PC] << 8 | this.memory[this.PC + 1];
+        return instruction;
     }
-    fetchOpcode() {
-        let opcode = this.memory[this.PC] << 8 | this.memory[this.PC + 1];
-        this.moveToNextInstruction();
-        return opcode;
-    }
-    executeOpcode(opcode) {
-        executeInstruction(this, opcode);
-    }
-   tick() {
+    tick() {
          // Fetch Opcode
-         const op = this.fetchOpcode();
-         const inst = this.executeOpcode(op);
-
-        // IMPLEMENT
+         const instruction = this.fetchInstruction();
+         this.state = reducerModule.step(this.state, instruction);
     }
+    loadGame(data) {
+        this.state = reducerModule.loadProgram(this.state, data);
+    }
+}
+
+var makeMachine = function(screenSize) {
+    var machine = new InterpreterMachine(screenSize);
+    return machine;
 }
