7b19e92cf51c9105b99600d022114151e6490166
Ricardo Murillo <ricardo.murillo@adroll.com>
2017-12-06 13:14:16 -0600

    Fix instruction

--- public/js/Chip8Machine.js
+++ public/js/Chip8Machine.js
@@ -362,7 +362,7 @@ class Chip8Machine {
                 this.I += this.V[reg];
                 break;
             case 0x29:
-                this.I = FONT_MEMORY_OFFSET + i * 5;
+                this.I = FONT_MEMORY_OFFSET + reg * 5;
                 break;
             case 0x33:
                 break;
