0c91f165f676a4193b7ef805b1d336e9714cbd2a
Ricardo Murillo <ricardo.murillo@adroll.com>
2017-12-07 12:03:37 -0600

    fix

--- public/js/Chip8Machine.js
+++ public/js/Chip8Machine.js
@@ -88,8 +88,8 @@ class Chip8Machine {
 
     // The maximum is inclusive and the minimum is inclusive
     getRandomInt(min, max) {
-        const min = Math.ceil(min);
-        const max = Math.floor(max);
+        min = Math.ceil(min);
+        max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
       }
 
