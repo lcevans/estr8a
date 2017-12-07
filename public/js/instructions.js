63a45bcea528df67413ef06d4cb36af900b1606d
Chris Evans <chris.evans@adroll.com>
2017-12-07 15:17:56 -0600

    Add instructions for games

---
+++ public/js/instructions.js
@@ -0,0 +1,135 @@
+// Game instructions go here. Pass your wisdom on to the next generation!
+gameInstructions = {
+    '15PUZZLE' : `
+Controls:<br>
+2 - Up<br>
+4 - Left<br>
+6 - Right<br>
+8 - Down<br>
+`,
+    BLINKY : `
+Controls:<br>
+3 - Up<br>
+6 - Down<br>
+7 - Left<br>
+8 - Right<br>
+`,
+    BLITZ : `
+Controls:<br>
+5 - Drop Bombs<br>
+`,
+    BRIX : `
+Controls:<br>
+4 - Left<br>
+6 - Right<br>
+`,
+    CONNECT4 : `
+Controls:<br>
+4 - Left<br>
+6 - Right<br>
+5 - Drop Chip<br>
+`,
+    GUESS : `
+??????????
+`,
+    HIDDEN : `
+Controls:<br>
+2 - Up<br>
+4 - Left<br>
+6 - Right<br>
+8 - Down<br>
+5 - Choose<br>
+`,
+    INVADERS : `
+Controls:<br>
+4 - Left<br>
+6 - Right<br>
+5 - Shoot<br>
+`,
+    KALEID : `
+Sit back and watch!
+`,
+    MAZE : `
+Just a maze. Gaze upon it and weep.
+`,
+    MERLIN : `
+Simon Clone <br>
+<br>
+Controls:<br>
+4, 5 - Buttons control the squares<br>
+7, 8<br>
+`,
+    MISSILE : `
+Controls:<br>
+8 - Shoot<br>
+`,
+    PONG : `
+Controls:<br>
+1 - Up<br>
+4 - Down<br>
+`,
+    PONG2 : `
+Controls:<br>
+1 - Up<br>
+4 - Down<br>
+`,
+    POLYBIUS : `
+Turn back now! You have been warned...
+`,
+    PUZZLE : `
+????????????
+`,
+    SYZYGY : `
+Snake Clone<br>
+<br>
+Controls:<br>
+E - Start<br>
+7 - Left<br>
+8 - Right<br>
+3 - Up<br>
+6 - Down<br>
+`,
+    TANK : `
+Controls:<br>
+2 - Down<br>
+4 - Left<br>
+6 - Right<br>
+8 - Up<br>
+5 - Shoot<br>
+`,
+    TETRIS : `
+Controls:<br>
+4 - Rotate<br>
+5 - Left<br>
+6 - Right<br>
+`,
+    TICTAC : `
+Controls:<br>
+1-9 form the 3x3 grid
+`,
+    UFO : `
+Controls:<br>
+4 - Shoot Left<br>
+5 - Shoot Mid<br>
+6 - Shoot Right<br>
+`,
+    VBRIX : `
+Controls:<br>
+1 - Up<br>
+4 - Down<br>
+`,
+    VERS : `
+2 Player Tron Clone<br>
+<br>
+Controls P1:<br>
+7,A,1,2 - Up,Down,Left,Right<br>
+<br>
+Controls P2:<br>
+C,D,B,F - Up,Down,Left,Right<br>
+`,
+    WIPEOFF : `
+Controls:<br>
+4 - Left<br>
+6 - Right<br>
+`,
+}
